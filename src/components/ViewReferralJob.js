51a10dca80ac5b1d1acdd8554e32e646aac72c64
saiviswateja <saiviswateja.teja@gmail.com>
2022-03-28 23:01:17 +0530

    added code for showing referred by detials in view referral job

--- src/components/ViewReferralJob.js
+++ src/components/ViewReferralJob.js
@@ -3,182 +3,194 @@ import {
   useParams
 } from "react-router-dom";
 import { getReferralById } from '../api-service/ReferralService';
-import {getLoggeduser, signOut} from '../helpers/utils';
+import { getLoggeduser, signOut } from '../helpers/utils';
 
 function ViewReferralJob() {
-  let {referral_id} = useParams();
+  let { referral_id } = useParams();
   const [referralDetails, setReferralDetails] = useState({});
   const [companyName, setCompanyName] = useState("");
   const [skills, setSills] = useState("");
   const [username, setUser] = useState("");
   const [userEmail, setUserEmail] = useState("");
 
-  useEffect(()=> { 
+  useEffect(() => {
     console.log("calling");
-      async function getReferral() {
-        let details = await getReferralById(referral_id);
-        setReferralDetails(details);
-        setCompanyName(details.company.name);
-        let skillsDetails = details.skills[0].name;
-        for(let i=1;i<skills.length;i++) {
-          skillsDetails = skillsDetails + i.name;
-        }
-        setSills(skillsDetails);
-        setUser(details.user.name);
-        setUserEmail(details.user.email);
+    async function getReferral() {
+      let details = await getReferralById(referral_id);
+      setReferralDetails(details);
+      setCompanyName(details.company.name);
+      let skillsDetails = details.skills[0].name;
+      for (let i = 1; i < skills.length; i++) {
+        skillsDetails = skillsDetails + i.name;
       }
-      getLoggeduser().then((user)=> {
-        if(user==null) {
-          signOut();
-          return;
-        }
-        getReferral();
-      });    
+      setSills(skillsDetails);
+      setUser(details.user.name);
+      setUserEmail(details.user.email);
+    }
+    getLoggeduser().then((user) => {
+      if (user == null) {
+        signOut();
+        return;
+      }
+      getReferral();
+    });
   }, []);
 
-  return ( <div className="container">
-  <h1 className="display-4 mt-5 mb-5">View Referral</h1>
-  <div className="jumbotron">
-    <div className="mb-3 row">
-      <label for="staticEmail" className="col-sm-2 col-form-label">
-        Role
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={referralDetails.role || ""}
-        />
+  return (<div className="container">
+    <div className='d-flex flex-row justify-content-between'>
+      <h1 className="display-4 mt-5 mb-5">View Referral</h1>
+      <div className='user_referral_details'>
+        {
+          referralDetails.user && 
+          <>
+          <h4>Referral By: {referralDetails.user.name || ""}</h4>
+        <h3>Email: {referralDetails.user.email || ""}</h3>
+        </>
+        }
+        {console.log(referralDetails)}
       </div>
     </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Company
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={companyName}
-        />
+    <div className="jumbotron">
+      <div className="mb-3 row">
+        <label for="staticEmail" className="col-sm-2 col-form-label">
+          Role
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={referralDetails.role || ""}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Company
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={companyName}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Minimum Experience
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={referralDetails.minExperience || ""}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Maximum Experience
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={referralDetails.maxExperience || ""}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Responsibilities
+        </label>
+        <div className="col-sm-10">
+          <textarea
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={referralDetails.responsibilities || ""}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Description
+        </label>
+        <div className="col-sm-10">
+          <textarea
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={referralDetails.description || ""}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Referral Code
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={referralDetails.referralCode || ""}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Skills Required
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={skills}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Referral By
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={skills}
+          />
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <label for="inputPassword" className="col-sm-2 col-form-label">
+          Skills Required
+        </label>
+        <div className="col-sm-10">
+          <input
+            type="text"
+            className="form-control"
+            id="inputPassword"
+            readOnly
+            value={skills}
+          />
+        </div>
       </div>
     </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Minimum Experience
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={referralDetails.minExperience || ""}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Maximum Experience
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={referralDetails.maxExperience || ""}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Responsibilities
-      </label>
-      <div className="col-sm-10">
-        <textarea
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={referralDetails.responsibilities || ""}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Description
-      </label>
-      <div className="col-sm-10">
-        <textarea
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={referralDetails.description || ""}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Referral Code
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={referralDetails.referralCode || ""}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Skills Required
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={skills}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Referral By
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={skills}
-        />
-      </div>
-    </div>
-    <div className="mb-3 row">
-      <label for="inputPassword" className="col-sm-2 col-form-label">
-        Skills Required
-      </label>
-      <div className="col-sm-10">
-        <input
-          type="text"
-          className="form-control"
-          id="inputPassword"
-          readOnly
-          value={skills}
-        />
-      </div>
-    </div>
-  </div>
-</div>);
+  </div>);
 }
 
 export default ViewReferralJob;
